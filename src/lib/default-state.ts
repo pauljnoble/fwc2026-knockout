7c84f5752f0ec33875a4b48399b7ad25166e73c3
Paul Noble <noble.pauljames@gmail.com>
2026-07-01 14:30:14 +1000

    Fix default state.

--- src/lib/default-state.ts
+++ src/lib/default-state.ts
@@ -5,6 +5,7 @@ export const DEFAULT_DRAW_STATE: DrawState = {
   "winners": {
     "0-pair-0": "BRA",
     "0-pair-1": "NOR",
+    "0-pair-2": "MEX",
     "0-pair-12": "MAR",
     "0-pair-13": "CAN",
     "0-pair-14": "FRA",
