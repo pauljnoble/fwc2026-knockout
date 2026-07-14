a69e354b6f96016d9beaf397fac7f4fc3fa782d2
Paul Noble <noble.pauljames@gmail.com>
2026-07-15 08:14:19 +1000

    Fix default state.

--- src/lib/default-state.ts
+++ src/lib/default-state.ts
@@ -30,6 +30,7 @@ export const DEFAULT_DRAW_STATE: DrawState = {
     "3-pair-0": "GB-ENG",
     "3-pair-1": "ARG",
     "3-pair-2": "ESP",
-    "3-pair-3": "FRA"
+    "3-pair-3": "FRA",
+    "4-pair-1": "ESP"
   }
 }
